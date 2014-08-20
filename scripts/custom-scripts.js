3dd79d20c908f5dcd4124f6b76d5921160d09a27
tomgelder <tomgelder@gmail.com>
2014-08-20 09:11:55 -0700

    header fun

--- scripts/custom-scripts.js
+++ scripts/custom-scripts.js
@@ -2,8 +2,3 @@ $(document).bind("mobileinit", function () {
       $.mobile.defaultPageTransition = 'slide';
 });
 
-//take iOS7 transparent menubar into account
-if (parseFloat(window.device.version) === 7.0) {
-    $('[data-role="header"]').addClass("ios7");
-    $('.ui-btn-left').addClass("ios7-header-button");
-};
